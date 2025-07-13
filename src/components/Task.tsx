b9a65276f50f317cade7befbd601759d622aead3
Jeff Nyman <jeffnyman@gmail.com>
2025-07-13 09:54:35 -0500

    Update Task component appearance.

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -27,14 +27,30 @@ presentation logic.
 
 export default function Task({ task: { id, title, state } }: TaskProps) {
   return (
-    <div className="list-item">
-      <label htmlFor={`title-${id}`} aria-label={title}>
+    <div className={`list-item ${state}`}>
+      <label
+        className="checkbox"
+        htmlFor={`archiveTask-${id}`}
+        aria-label={`archiveTask-${id}`}
+      >
+        <input
+          id={`archiveTask-${id}`}
+          name="checked"
+          type="checkbox"
+          disabled={true}
+          checked={state === "TASK_ARCHIVED"}
+        ></input>
+        <span></span>
+      </label>
+
+      <label className="title" htmlFor={`title-${id}`} aria-label={title}>
         <input
           id={`title-${id}`}
           name="title"
           type="text"
-          value={title + state}
+          value={title}
           readOnly={true}
+          placeholder="Input title"
         ></input>
       </label>
     </div>
