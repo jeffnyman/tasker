d25152c2ce451130f0fb0d6e76614c2b70b7cfc9
Jeff Nyman <jeffnyman@gmail.com>
2025-07-13 08:54:47 -0500

    Add default Task story.

---
+++ src/components/Task.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import Task from "./Task";
+
+/*
+The `meta` object will be the default export that describes
+the component.
+*/
+
+const meta = {
+  component: Task,
+  title: "Core/Task",
+  tags: ["autodocs"],
+} satisfies Meta<typeof Task>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/*
+The following are named exports that describe the stories
+for the component.
+*/
+
+export const Default: Story = {
+  args: {
+    task: {
+      id: "1",
+    },
+  },
+};
